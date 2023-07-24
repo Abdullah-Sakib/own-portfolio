36acbe390978e2898ba2a8ab129d75b9bee8969f
Abdullah-Sakib <abdullahsakib888@gmail.com>
2023-07-24 20:45:41 +0600

    feat: Added background color  in the whole app

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./sidebar";
 
 const MainLayout = () => {
   return (
-    <div className="flex  custom-scrollbar">
+    <div className="flex  custom-scrollbar bg-[#49d49d10]">
       <aside className="h-screen w-[20vw] sticky  top-0  border-r-[1px] border-green-400 border-opacity-40  overflow-auto custom-scrollbar-sidebar">
         <Sidebar />
       </aside>
